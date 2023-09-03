32b08ecfc8031024cb6e3c0deebc177f24ac80c9
chiranthakaj <chiransj@gmail.com>
2023-09-03 21:30:28 +0530

    First Commit

---
+++ Mastering SharePoint Framework SPFx Development/test174/src/webparts/test174Webpartdemo/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface ITest174WebpartdemoWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'Test174WebpartdemoWebPartStrings' {
+  const strings: ITest174WebpartdemoWebPartStrings;
+  export = strings;
+}
