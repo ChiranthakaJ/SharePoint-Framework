548f604395f09c83ee332837be1c3504c49be8a1
chiranthakaj <chiransj@gmail.com>
2024-10-06 13:41:41 +1000

    First commit 06-10-2024

---
+++ Microsoft Official Tutorials/First Basic Web Part/src/webparts/firstBasicWebPart/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IFirstBasicWebPartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'FirstBasicWebPartWebPartStrings' {
+  const strings: IFirstBasicWebPartWebPartStrings;
+  export = strings;
+}
