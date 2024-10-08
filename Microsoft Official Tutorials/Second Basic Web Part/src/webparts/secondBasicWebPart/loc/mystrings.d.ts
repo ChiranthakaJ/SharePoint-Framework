0a3c3ea9630471b4b9cdd77a1d57f56f7089eeaf
chiranthakaj <chiransj@gmail.com>
2024-10-09 01:30:27 +1000

    First Commit 08-10-2024

---
+++ Microsoft Official Tutorials/Second Basic Web Part/src/webparts/secondBasicWebPart/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface ISecondBasicWebPartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'SecondBasicWebPartWebPartStrings' {
+  const strings: ISecondBasicWebPartWebPartStrings;
+  export = strings;
+}
